234f550b32c105c3a2dc049d10116bf990f43c00
lamchiwing <60570944+lamchiwing@users.noreply.github.com>
2022-01-19 10:20:04 +0100

    Update and rename README.md to student_grade.py

---
+++ student_grade.py
@@ -0,0 +1,7 @@
+def student_grade(name,grade):
+    format = "{} received {}% on the exam".format(name,grade)
+    return format
+    
+    print(student_grade("Reed",80))
+    print(student_grade("Paige",92))
+    print(student_grade("Jessie",85))
